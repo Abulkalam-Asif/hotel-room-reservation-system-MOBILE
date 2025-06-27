f9495e410b249d1d7c2600930d683fc22d39117e
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2025-06-27 23:05:56 +0500

    .env removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
   runApp(const MyApp());
 }
 
